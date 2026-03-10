a163a375c1638d0a624cab4bbfdfa652eca9060f
Anna Moore <annathekarategirl@gmail.com>
2026-03-10 14:02:09 -0400

    v

---
+++ test.js
@@ -0,0 +1,26 @@
+async function fetchJSONData(path) {
+    const response = await fetch(path);
+    console.log(response)
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+}
+let dialogNum = 0;
+
+function getCharacterJPG(characterName)
+{
+    switch(characterName){
+        case "Special Week":
+            return "specialweek.jpg"
+    }
+}
+async function main() {
+    const data = await fetchJSONData('./beginning.json');
+    console.log(data);
+    console.log(Object.keys(data['dialog'][dialogNum]));
+}
+
+main();
