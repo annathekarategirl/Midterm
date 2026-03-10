c5bd0f1d92762871a6ea93e07c067e1d8c2bbf64
annathekarategirl <annathekarategirl@gmail.com>
2026-03-10 15:37:26 -0400

    f

--- test.js
+++ test.js
@@ -19,13 +19,24 @@ function getCharacterJPG(characterName)
 }
 async function main(string) {
     const data = await fetchJSONData(string);
-    console.log(data);
-    console.log(Object.keys(data['dialog'][dialogNum]));
+    //console.log(data);
+    //console.log(Object.keys(data['dialog'][dialogNum]));
 }
 
 //main('./ofcourse.json');
 const start =document.getElementById("start")
 start.addEventListener("click", story)
+
+const beginning=main("./beginning.json")
+console.log(beginning)
+function begin(){
+
+}
+
+
+
+
+
 
 function story(){
 //call all functions here for the seamless story
